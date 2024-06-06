7e735576e25d0cbc44f0a5cb5f67aa68fd12792d
Shreyas4545 <shreyasjakati66@gmail.com>
2024-06-06 08:39:22 +0530

    Added ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  // Use the ConfigService to get the port
+  const port = configService.get<number>('PORT') || 6000;
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
