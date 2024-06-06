3c822e331c87037ae27ffc547c1b953f2c5991ce
Shreyas4545 <shreyasjakati66@gmail.com>
2024-06-06 18:48:51 +0530

    Chnages done

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   // Use the ConfigService to get the port
-  const port = configService.get<number>('PORT') || 6000;
+  const port = process.env.PORT || 10000;
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
