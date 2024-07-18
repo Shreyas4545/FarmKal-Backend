7af01a9564e4cc548bc0401fd55b8cc5f6eada7a
Shreyas4545 <shreyasjakati66@gmail.com>
2024-07-18 20:03:02 +0530

    Changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Use the PORT environment variable provided by Render
+  // Use the PORT environment variable providped by Render
   const port = process.env.PORT || 3000;
   const host = '0.0.0.0';
 
