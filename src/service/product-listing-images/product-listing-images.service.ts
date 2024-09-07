7e99b56f09759cf80c4adcb00f2789d59e874164
Shreyas4545 <shreyasjakati66@gmail.com>
2024-09-07 22:45:19 +0530

    Changes

--- src/service/product-listing-images/product-listing-images.service.ts
+++ src/service/product-listing-images/product-listing-images.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { FirebaseService } from 'src/utils/imageUpload';
+import { FirebaseService } from '../../utils/imageUpload';
 
 @Injectable()
 export class ImagesService {
