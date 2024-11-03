d7f72182bbca35a620edff69a430eff4a18fcd77
Shreyas4545 <shreyasjakati66@gmail.com>
2024-11-03 22:33:03 +0530

    Changes

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthInterceptor } from './Interceptors/authentication.interceptor';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,9 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(express.json({ limit: '250mb' }));
   app.use(express.urlencoded({ limit: '250mb' }));
+
+  app.use(bodyParser.json({ limit: '100mb' }));
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
   app.enableCors();
   await app.listen(port, host);
   console.log(`Application is running on: http://${host}:${port}`);
